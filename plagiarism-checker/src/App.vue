6724e003e11053a8b512c454fad3d100ae3ec4d2
Tsalits <tsalissabilafatah@gmail.com>
2025-02-01 21:39:54 +0700

    ya

--- plagiarism-checker/src/App.vue
+++ plagiarism-checker/src/App.vue
@@ -1,35 +1,29 @@
 <script setup>
 import tes1 from './components/tes1.vue'; 
-import HelloWorld from './components/HelloWorld.vue'
+import SEOChecker from './components/SEOChecker.vue';
 </script>
 
 <template>
-<<<<<<< HEAD
+
   <div id="app">
     <tes1 />
   </div>
-=======
->>>>>>> 15c39d3e88d71c12eb38202ae106b92c256c3516
-  <div>
+  <div id="">
     <SEOChecker />
   </div>
 </template>
 
 <script>
-import SEOChecker from './components/SEOChecker.vue';
 
-export default {
-  components: {
-    SEOChecker
-<<<<<<< HEAD
-  }
-};
+  export default {
+    components: {
+      SEOChecker
+    }
+  };
 </script>
 
 <style scoped>
-=======
-  }
-};
-</script>
->>>>>>> 15c39d3e88d71c12eb38202ae106b92c256c3516
+
+</style>
+
 
