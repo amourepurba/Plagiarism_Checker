8b08c086feeaedbd57fc2047305e37ffe7e50c99
TAMA <auzan.tama@gmail.com>
2025-03-27 11:52:22 +0700

    sme

--- plagiarism-checker/backend/migrations/20250206171038-create-user.js
+++ plagiarism-checker/backend/migrations/20250206171038-create-user.js
@@ -1,12 +1,11 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
 exports.up = function(knex) {
   return knex.schema.createTable('users', function(table) {
     table.increments('id').primary();
+    table.string('username', 255).notNullable();
     table.string('email', 255).notNullable().unique();
     table.string('password', 255).notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now()).alter();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).onUpdate(knex.fn.now());
   });
 };
 
