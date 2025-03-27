94be88c21f4ed5bf831e55949432f70c90ed858c
TAMA <auzan.tama@gmail.com>
2025-03-27 12:29:50 +0700

    server

--- plagiarism-checker/backend/server.js
+++ plagiarism-checker/backend/server.js
@@ -123,5 +123,5 @@ app.get('/profile', verifyToken, (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => console.log(`Server berjalan pada port ${PORT}`));
