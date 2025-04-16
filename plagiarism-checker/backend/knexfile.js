19bab0b1fff113a03f0f1fa2b5d28643fbed54ba
amril <amrilazhar@gmail.com>
2025-04-16 14:38:17 +0700

    update gitignore

--- plagiarism-checker/backend/knexfile.js
+++ plagiarism-checker/backend/knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
 	development: {
 		client: "mysql2",
 		connection: {
-			host: "localhost",
-			user: "root",
-			password: "@Auzan1234",
-			database: "seoplagiarismchecker",
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
 		},
 		migrations: {
 			directory: "./migrations",
