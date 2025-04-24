62d3f30296254a5d3177cdf521a8363dc684af53
amril <amrilazhar@gmail.com>
2025-04-24 10:46:47 +0700

    adjust vite config

--- plagiarism-checker/Frontend/vite.config.js
+++ plagiarism-checker/Frontend/vite.config.js
@@ -1,28 +1,35 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/check-text': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/check-file': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/check-url': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-})
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	const TARGET_URL = `${env.VITE_TARGET_URL ?? "http://localhost:3000"}`;
+
+	return {
+		plugins: [vue()],
+
+		server: {
+			proxy: {
+				"/check-text": {
+					target: TARGET_URL,
+					changeOrigin: true,
+				},
+				"/check-file": {
+					target: TARGET_URL,
+					changeOrigin: true,
+				},
+				"/check-url": {
+					target: TARGET_URL,
+					changeOrigin: true,
+				},
+				"/api": {
+					target: TARGET_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
+			},
+		},
+	};
+});
